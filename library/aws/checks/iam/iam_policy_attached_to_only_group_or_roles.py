451ceb9f153adfd2dd3e74b1d078dff83ce92018
mohammad-asif-comprinno <167754572+mohammad-asif-comprinno@users.noreply.github.com>
2024-12-18 09:15:28 +0530

    Update iam_policy_attached_to_only_group_or_roles.py

--- library/aws/checks/iam/iam_policy_attached_to_only_group_or_roles.py
+++ library/aws/checks/iam/iam_policy_attached_to_only_group_or_roles.py
@@ -14,23 +14,23 @@ class iam_policy_attached_to_only_group_or_roles(Check):
         client = connection.client('iam')
 
         try:
-            # List all IAM users
+            
             users = client.list_users()['Users']
 
-            # Check for attached policies for each user
+            
             for user in users:
                 username = user['UserName']
-                # List attached user policies
+              
                 attached_policies = client.list_attached_user_policies(UserName=username)['AttachedPolicies']
 
                 if attached_policies:
 
-                    report.resource_ids_status[username] = True  # Flag as a violation
+                    report.resource_ids_status[username] = False  
                 else:
 
-                    report.resource_ids_status[username] = False  # No violation
+                    report.resource_ids_status[username] = True 
 
-            # Overall check passes if no user has directly attached policies
+
             report.passed = not any(report.resource_ids_status.values())
 
         except Exception as e:
@@ -38,4 +38,6 @@ class iam_policy_attached_to_only_group_or_roles(Check):
             report.passed = False
 
         return report
+
+
 
