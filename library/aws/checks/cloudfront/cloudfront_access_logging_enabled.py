a0059d99c9da1e80b7aaa07e1b0c6faa3985e651
supriyo <supriyo.bhakat@comprinno.net>
2025-01-03 18:50:28 +0530

    added get_distribution() api call for getch the complete configuration

--- library/aws/checks/cloudfront/cloudfront_access_logging_enabled.py
+++ library/aws/checks/cloudfront/cloudfront_access_logging_enabled.py
@@ -1,7 +1,7 @@
 """
-AUTHOR: SUPRIYO BHAKAT
+AUTHOR: Supriyo Bhakat
 EMAIL: supriyo.bhakat@comprinno.net
-DATE: 2025-03-01
+DATE: 2024-01-02
 """
 
 import boto3
@@ -15,16 +15,22 @@ class cloudfront_access_logging_enabled(Check):
         response = client.list_distributions()
         return response.get('DistributionList', {}).get('Items', [])
 
-    def _check_logging_enabled(self, distribution):
-        distribution_id = distribution['Id']
-        dist_config = distribution.get('DistributionConfig', {})
+    def _get_distribution_config(self, client, distribution_id):
+        try:
+            response = client.get_distribution_config(Id=distribution_id)
+            return response.get('DistributionConfig', {})
+        except (ClientError, BotoCoreError):
+            return {}
+
+    def _check_logging_enabled(self, client, distribution_id):
+        dist_config = self._get_distribution_config(client, distribution_id)
         dist_logging = dist_config.get('Logging', {})
-        return distribution_id, dist_logging.get('Enabled', False)
+        return dist_logging.get('Enabled', False)
 
     def execute(self, connection: boto3.Session) -> CheckReport:
         client = connection.client('cloudfront')
         report = CheckReport(name=__name__)
-        report.passed = False  # Start with False
+        report.passed = False
 
         try:
             distributions = self._get_distributions(client)
@@ -33,15 +39,14 @@ class cloudfront_access_logging_enabled(Check):
                 report.resource_ids_status['NoDistributions'] = False
                 return report
 
-            # Start with assumption all distributions will pass
             all_distributions_logging = True
 
             for distribution in distributions:
                 try:
-                    dist_id, logging_enabled = self._check_logging_enabled(distribution)
+                    dist_id = distribution['Id']
+                    logging_enabled = self._check_logging_enabled(client, dist_id)
                     report.resource_ids_status[dist_id] = logging_enabled
 
-                    # If logging is disabled for any distribution, mark for failure
                     if not logging_enabled:
                         all_distributions_logging = False
 
@@ -49,15 +54,11 @@ class cloudfront_access_logging_enabled(Check):
                     report.resource_ids_status[distribution.get('Id', 'Unknown')] = False
                     all_distributions_logging = False
 
-            # Set final pass/fail status after checking all distributions
             report.passed = all_distributions_logging
 
         except (ClientError, BotoCoreError, Exception):
-            # Handle AWS API errors or other exceptions that occur during execution
             report.passed = False
             return report
 
         return report
-
-
 
