4ed677f66650e4f58a73db379bc76b9a5e2f5430
deepak-puri-comprinno <deepak.puri@comprinno.net>
2025-01-14 19:20:14 +0530

    added ec2_security_group_default_restrict_traffic

--- library/aws/checks/ec2/ec2_security_group_default_restrict_traffic.py
+++ library/aws/checks/ec2/ec2_security_group_default_restrict_traffic.py
@@ -1,48 +1,61 @@
 """
 AUTHOR: Deepak Puri
 EMAIL: deepak.puri@comprinno.net
-DATE: 2024-10-09
+DATE: 2025-01-14
 """
-        
+
 import boto3
 from tevico.engine.entities.report.check_model import CheckReport
 from tevico.engine.entities.check.check import Check
 
+
 class ec2_security_group_default_restrict_traffic(Check):
 
     def execute(self, connection: boto3.Session) -> CheckReport:
-        report = CheckReport(name=__name__)
         client = connection.client('ec2')
-        security_groups = client.describe_security_groups()['SecurityGroups']
-        vpcs = client.describe_vpcs()['Vpcs']
-        network_interfaces = client.describe_network_interfaces()['NetworkInterfaces']
-        report.passed = True
-        vpcs_in_use = {vpc['VpcId']: vpc for vpc in vpcs}
-        sg_network_interfaces = {}
-        for ni in network_interfaces:
-            for group in ni['Groups']:
-                sg_id = group['GroupId']
-                if sg_id not in sg_network_interfaces:
-                    sg_network_interfaces[sg_id] = []
-                sg_network_interfaces[sg_id].append(ni['NetworkInterfaceId'])
 
-        for sg in security_groups:
-            sg_id = sg['GroupId']
-            sg_name = sg['GroupName']
-            vpc_id = sg.get('VpcId')
-            
-            if sg_name == 'default':
-                sg_in_use = vpc_id in vpcs_in_use  
-                has_network_interfaces = sg_id in sg_network_interfaces and len(sg_network_interfaces[sg_id]) > 0                
-                if sg_in_use and has_network_interfaces:
+        # Initialize the report
+        report = CheckReport(name=__name__)
+        report.passed = True
+        report.resource_ids_status = {}
+
+        try:
+            # Fetch all security groups with pagination
+            security_groups = []
+            next_token = None
+            while True:
+                if next_token:
+                    response = client.describe_security_groups(NextToken=next_token)
+                else:
+                    response = client.describe_security_groups()
+                security_groups.extend(response.get('SecurityGroups', []))
+                next_token = response.get('NextToken')
+                if not next_token:
+                    break
+
+            # Check each security group for default groups in all VPCs
+            for sg in security_groups:
+                sg_id = sg['GroupId']
+                sg_name = sg['GroupName']
+                vpc_id = sg.get('VpcId', 'N/A')
+
+                # Only process default security groups
+                if sg_name == 'default':
                     ingress_rules = sg.get('IpPermissions', [])
                     egress_rules = sg.get('IpPermissionsEgress', [])
+
                     if not ingress_rules and not egress_rules:
-                        report.resource_ids_status[sg_id] = True
+                        report.resource_ids_status[
+                            f"{sg_id} (default SG) in VPC {vpc_id} is properly restricted"
+                        ] = True
                     else:
-                        report.resource_ids_status[sg_id] = False
+                        report.resource_ids_status[
+                            f"{sg_id} (default SG) in VPC {vpc_id} allows traffic"
+                        ] = False
                         report.passed = False
-                else:
-                    report.resource_ids_status[sg_id] = True
+
+        except Exception as e:
+            report.resource_ids_status["Error fetching security groups"] = False
+            report.passed = False
 
         return report
